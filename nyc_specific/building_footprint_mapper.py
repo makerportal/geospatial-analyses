b8e1d0a577d273483b99ec8569a934ff2a0eaa86
Joshua Hrisko <49917891+josh-hrisko@users.noreply.github.com>
2020-09-04 19:08:23 -0400

    Update building_footprint_mapper.py

--- nyc_specific/building_footprint_mapper.py
+++ nyc_specific/building_footprint_mapper.py
@@ -11,7 +11,6 @@
 #
 ################################################################
 #
-import time
 import cartopy.io.shapereader as shpreader
 import cartopy.crs as ccrs
 from cartopy.io.shapereader import Reader
